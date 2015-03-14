fda3cc4fdfd7f355d4280b1473f189bfe7bb52b8
lichencai <793385991@qq.com>
2015-03-14 17:09:38 +0800

    1

---
+++ src/main/java/hibernate/EventManager.java
@@ -0,0 +1,31 @@
+package hibernate;
+
+import hibernate.entity.Event;
+import hibernate.util.HibernateUtil;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+public class EventManager {
+	public static void main(String[] args) {
+        EventManager mgr = new EventManager();
+
+        
+        mgr.createAndStoreEvent("My Event", new Date());
+
+        HibernateUtil.getSessionFactory().close();
+    }
+
+    private void createAndStoreEvent(String title, Date theDate) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+
+        Event theEvent = new Event();
+        theEvent.setTitle(title);
+        theEvent.setDate(theDate);
+        session.save(theEvent);
+
+        session.getTransaction().commit();
+    }
+}
